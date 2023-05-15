2948bfcb569714b1e19d671bb877f2b3f921b26d
mahim <mahim216@gmail.com>
2023-05-15 05:21:46 -0400

    removing unused variable

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import { wordList } from './modules/wordList.js';
 
 export const filteredWords = wordList.filter(word => word.length <= 11);
 
-export const blanksContainer = document.querySelector('.blank');
-
 const game = new Game();
 
 game.start();
