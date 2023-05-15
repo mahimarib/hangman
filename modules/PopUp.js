bdec72de7c5309659781693d58bd6b324c0c6676
mahim <mahim216@gmail.com>
2023-05-15 10:16:16 -0400

    moving styling to css

--- modules/PopUp.js
+++ modules/PopUp.js
@@ -1,6 +1,5 @@
 export default function PopUp() {
     const element = document.querySelector('.pop-out');
-    const box = document.querySelector('.pop-out .container');
     const title = document.querySelector('.pop-out h1');
     const p = document.querySelector('.pop-out p');
     const button = document.querySelector('.pop-out button');
@@ -22,17 +21,11 @@ export default function PopUp() {
     };
 
     this.appear = () => {
-        element.style.zIndex = '2';
-        element.style.opacity = '1';
-        element.backdropFilter = 'blur(3px)';
-        box.style.opacity = '1';
-        box.style.transform = 'translateY(0)';
+        element.classList.add('appear');
     };
 
     this.hide = () => {
-        element.style.zIndex = '-100';
-        box.style.transform = 'translateY(-20px)';
-        box.style.opacity = '0';
+        element.classList.remove('appear');
     };
 
     button.addEventListener('click', () => {
