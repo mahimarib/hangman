4b5c4c44bd2b4e55cdc834920e99fc25dc0303a3
mahim <mahim216@gmail.com>
2023-05-19 02:51:40 -0400

    added mobile view

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import Game from './modules/Game.js';
 import { wordList } from './modules/wordList.js';
 
-export const filteredWords = wordList.filter(word => word.length <= 11);
+const box = document.querySelector('aside').getBoundingClientRect();
+
+export const filteredWords = wordList.filter(
+    word => word.length <= Math.floor(box.width / 45)
+);
 
 const game = new Game();
 
